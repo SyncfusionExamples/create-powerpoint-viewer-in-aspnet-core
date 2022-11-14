6d675ab2cdd2d582b466b34dc6eaad7be7ae018c
shaikhazara.shaikusmanahamed <shaikhazara.shaikusmanahamed@syncfusion.com>
2022-11-14 11:45:47 +0530

    Added PowerPointViewer sample

---
+++ PowerPointViewer/.NET-Standard/Controllers/PowerPointViewerController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using Syncfusion.EJ2.PdfViewer;
+using Syncfusion.Pdf;
+using Syncfusion.Presentation;
+using Syncfusion.PresentationToPdfConverter;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace PowerPointViewer.Controllers
+{
+    public class PowerPointViewerController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private IMemoryCache _cache;
+
+        public PowerPointViewerController(IHostingEnvironment hostingEnvironment, IMemoryCache cache)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _cache = cache;
+        }
+
+        [AcceptVerbs("Post")]
+        [HttpPost]
+        [Route("api/[controller]/Load")]
+        public IActionResult Load([FromBody] Dictionary<string, string> jsonObject)
+        {
+            PdfRenderer pdfviewer = new PdfRenderer(_cache);
+            MemoryStream stream = new MemoryStream();
+            object jsonResult = new object();
+            if (jsonObject != null && jsonObject.ContainsKey("document"))
+            {
+                if (bool.Parse(jsonObject["isFileName"]))
+                {
+                    string documentPath = GetDocumentPath(jsonObject["document"]);
+                    if (!string.IsNullOrEmpty(documentPath))
+                    {
+                        byte[] bytes = System.IO.File.ReadAllBytes(documentPath);
+                        stream = new MemoryStream(bytes);
+                    }
+                    else
+                    {
+                        return this.Content(jsonObject["document"] + " is not found");
+                    }
+                }
+                else
+                {
+                    byte[] bytes = Convert.FromBase64String(jsonObject["document"]);
+                    stream = new MemoryStream(bytes);
+                }
+            }
+
+            try
+            {
+                //Open the PowerPoint presentation using Syncfusion Presentation library
+                IPresentation presentation = Presentation.Open(stream);
+                //Convert a PowerPoint presentation as PDF to view the generated PDF file in our Syncfusion PdfViewer
+                PdfDocument pdfDocument = PresentationToPdfConverter.Convert(presentation);
+                stream.Dispose();
+                //Save the converted Pdf document to get a physical DOM of PDF document.
+                stream = new MemoryStream();
+                pdfDocument.Save(stream);
+                //Dispose the pdf and presentation instance.
+                pdfDocument.Dispose();
+                presentation.Dispose();
+                //Reset the pdf stream position.
+                stream.Position = 0;
+            }
+            catch
+            {
+
+            }
+
+            jsonResult = pdfviewer.Load(stream, jsonObject);
+            return Content(JsonConvert.SerializeObject(jsonResult));
+        }
+
+        [AcceptVerbs("Post")]
+        [HttpPost]
+        [Route("api/[controller]/Unload")]
+        public IActionResult Unload([FromBody] Dictionary<string, string> jsonObject)
+        {
+            PdfRenderer pdfviewer = new PdfRenderer(_cache);
+            pdfviewer.ClearCache(jsonObject);
+            return this.Content("Document cache is cleared");
+        }
+
+        [AcceptVerbs("Post")]
+        [HttpPost]
+        [Route("api/[controller]/RenderPdfPages")]
+        public IActionResult RenderPdfPages([FromBody] Dictionary<string, string> jsonObject)
+        {
+            PdfRenderer pdfviewer = new PdfRenderer(_cache);
+            object jsonResult = pdfviewer.GetPage(jsonObject);
+            return Content(JsonConvert.SerializeObject(jsonResult));
+        }
+
+        [AcceptVerbs("Post")]
+        [HttpPost]
+        [Route("api/[controller]/RenderThumbnailImages")]
+        public IActionResult RenderThumbnailImages([FromBody] Dictionary<string, string> jsonObject)
+        {
+            PdfRenderer pdfviewer = new PdfRenderer(_cache);
+            object result = pdfviewer.GetThumbnailImages(jsonObject);
+            return Content(JsonConvert.SerializeObject(result));
+        }
+        [AcceptVerbs("Post")]
+        [HttpPost]
+        [Route("api/[controller]/Download")]
+        public IActionResult Download([FromBody] Dictionary<string, string> jsonObject)
+        {
+            PdfRenderer pdfviewer = new PdfRenderer(_cache);
+            string documentBase = pdfviewer.GetDocumentAsBase64(jsonObject);
+            return Content(documentBase);
+        }
+        [AcceptVerbs("Post")]
+        [HttpPost]
+        [Route("api/[controller]/PrintImages")]
+        public IActionResult PrintImages([FromBody] Dictionary<string, string> jsonObject)
+        {
+            PdfRenderer pdfviewer = new PdfRenderer(_cache);
+            object pageImage = pdfviewer.GetPrintImage(jsonObject);
+            return Content(JsonConvert.SerializeObject(pageImage));
+        }
+        private string GetDocumentPath(string document)
+        {
+            string documentPath = string.Empty;
+            if (!System.IO.File.Exists(document))
+            {
+                string basePath = _hostingEnvironment.WebRootPath;
+                string dataPath = string.Empty;
+                dataPath = basePath + @"/";
+                if (System.IO.File.Exists(dataPath + document))
+                    documentPath = dataPath + document;
+            }
+            else
+            {
+                documentPath = document;
+            }
+            return documentPath;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# create-powerpoint-viewer-in-aspnet-core
-This repo contains an ASP.NET Core example to view the PowerPoint files (convert PowerPoint as PDF and view it using PDF Viewer)
+# PowerPoint Viewer in C#
+
+This repository contains examples that illustrates how to view the PowerPoint Presentation through PdfViewer in C# using Syncfusion [.NET&nbsp;PowerPoint library](https://www.syncfusion.com/document-processing/powerpoint-framework/net/powerpoint-library) (Presentation). This can achieved this by converting a PowerPoint presentations (PPTX) to PDF and then you can view that in PDF Viewer.
+
+# Key Features
+
+-	[PowerPoint Viewer](PowerPointViewer/) - View the PowerPoint presentation through PdfViewer by converting it as PDF.
+
+In PowerPoint viewer, you can achieve the below functionality in the application.
+-	Display PowerPoint slides.
+-	Download and Print a PDF document.
+
+
+# Resources
+
+- **Product page:** [Syncfusion PowerPoint Framework](https://www.syncfusion.com/document-processing/powerpoint-framework/net)
+- **Documentation:** [Syncfusion PowerPoint library](https://help.syncfusion.com/file-formats/presentation/presentation-to-pdf)
+- **Online demo:** [Syncfusion PowerPoint library - Online demos](https://ej2.syncfusion.com/aspnetcore/PowerPoint/PowerPointViewer#/fluent)
+
+# How to run the examples
+
+-	Download this project to a location in your disk.
+-	Open the solution file using Visual Studio.
+-	Rebuild the solution to install the required NuGet packages.
+-	Run the application.
+
+# Syncfusion .NET PowerPoint Library
+The Syncfusion [.NET PowerPoint library](https://www.syncfusion.com/document-processing/powerpoint-framework/net/powerpoint-library) (Presentation) allows you to add advanced PowerPoint presentation processing functionalities to any .NET application and does not require Microsoft PowerPoint application to be installed in the machine. It is a non-UI component that provides a full-fledged PowerPoint presentation instance model similar to the Microsoft Office COM libraries to iterate with the presentation elements explicitly and perform necessary manipulation. Take a moment to peruse the [documentation](https://help.syncfusion.com/file-formats/presentation/getting-started), where you can find basic PowerPoint presentation processing options along with the features like create and edit [charts](https://help.syncfusion.com/file-formats/presentation/working-with-charts), [clone](https://help.syncfusion.com/file-formats/presentation/working-with-slide#cloning-slide) and [merge](https://help.syncfusion.com/file-formats/presentation/working-with-slide#merging-slide) slides in Presentation, create and edit [animations](https://help.syncfusion.com/file-formats/presentation/working-with-animation#edit-existing-animation-effect) and [transistion](https://help.syncfusion.com/file-formats/presentation/create-edit-slide-transitions-in-powerpoint-presentation-slides-cs-vb-net#modify-a-transition-effect-applied-to-a-powerpoint-slide) effects, [protect](https://help.syncfusion.com/file-formats/presentation/security) the PowerPoint Presentation, and most importantly, the [PDF](https://help.syncfusion.com/file-formats/presentation/presentation-to-pdf) and [Image](https://help.syncfusion.com/file-formats/presentation/presentation-to-image) conversions with code examples.
+
+# Support and feedback
+For any other queries, reach our [Syncfusion support team](https://support.syncfusion.com/) or post the queries through the [community forums](https://www.syncfusion.com/forums).
+
+Request new feature through [Syncfusion feedback portal](https://www.syncfusion.com/feedback).
+
+
